9c871e9a4b717fdcb373ceefdf52ec9c207dce17
juanfelipegarcia <jfgarcia064@misena.edu.co>
2021-09-15 02:28:56 -0500

    1° Se agrega el projecto

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Conversor de modena",
+    subtitle: "Conversor Con conexion a API",
+    description:
+      "........................................................... ..............................................................",
+    image: "./conversor1.gif",
+    link: "https://reactjs.org",
+  },
+  {
+    title: "Conversor miltimonedas",
+    subtitle: "Conversor multimonedas Con conexion a API",
+    description:
+      "........................................................... ..............................................................",
+    image: "./conversormonedas1.gif",
+    link: "https://reactjs.org",
+  },
+  {
+    title: "Abcedario y numeros en Ingles",
+    subtitle: "Juego para niños",
+    description:
+      "........................................................... ..............................................................",
+    image: "./abc1.gif",
+    link: "https://reactjs.org",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "........................................................... ..............................................................",
+    image: "./image.png",
+    link: "https://reactjs.org",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Espacio reservado para las experiencias de nuestros futuros clientes donde contaran la experiencia  de trabajar con  nuestro grupo de trabajo.",
+    image: "./progra2.jpg",
+    name: "Futuros clientes",
+    company: "Kreemo",
+  },
+  {
+    quote:
+      "Espacio reservado para las experiencias de nuestros futuros clientes donde contaran la experiencia  de trabajar con  nuestro grupo de trabajo.",
+    image: "./progra1.jpg",
+    name: "Futuros clientes",
+    company: "Kreemo",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "PHP",
+  "HTML",
+  "Bootstrap",
+  "CSS",
+];
